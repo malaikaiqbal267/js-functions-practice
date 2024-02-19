3adcd45f71c69380e173cf8584c5cb90b9dbdaa0
malaikaiqbal267 <malaikaiqbal267@gmail.com>
2024-02-19 22:35:48 +0500

    done

---
+++ index.js
@@ -0,0 +1,49 @@
+// Question # 1
+// Write a function called calculateTotalCost that takes two arguments: 
+// price (the price of an item) and quantity (the quantity of the item purchased).
+//  The function should calculate and return the total cost by multiplying the price and quantity. 
+//  Test the function with different price and quantity values.
+
+// function calculateTotalCost(price, quantity) {
+//     return price * quantity;
+//   }
+// console.log(calculateTotalCost(7, 2)); 
+
+// Question # 2
+// Develop a function called calculateArea that accepts two arguments:
+//  length and width of a rectangle. The function should calculate and return 
+//  the area of the rectangle using the formula: area = length * width. 
+//  Test the function with various length and width values.
+
+// function calculateArea(length, width) {
+//     return length * width;
+//   }
+// console.log(calculateArea(3, 4)); 
+
+// Question # 3
+// Write a function called calculateBMI that takes two arguments: 
+// weight (in kilograms) and height (in meters). The function should 
+// calculate and return the BMI using the formula: BMI = weight / (height * height). 
+// Test the function with different weight and height values.
+
+// function calculateBMI(weight, height) {
+//     return weight / (height * height);
+//   }
+// console.log(calculateBMI(70, 1.7)); 
+
+// Question # 4
+// Develop a function named calculateTriangleArea that accepts three arguments:
+//  base, height, and side. The function should calculate and return the area of a triangle
+//  using the formula: area = (base * height) / 2. Test the function with various base, 
+// height, and side values.
+function calculateTriangleArea(base, height) {
+    return (base * height) / 2;
+  }
+  
+  // Testing the function
+  console.log(calculateTriangleArea(3, 3)); 
+
+
+ 
+  
+ 
